53db4b28ddab0a71c437da98419291135e3a6aed
darkhist <qmsalas@iastate.edu>
2019-04-15 13:26:29 -0500

    fix: add x-header (#2)

--- src/web_components/index.js
+++ src/web_components/index.js
@@ -1,4 +1,5 @@
 import XApp from './XApp';
+import XHeader from './XHeader';
 import XButton from './XButton';
 import XItem from './XItem';
 import XItemContainer from './XItemContainer';
@@ -6,6 +7,7 @@ import XItemRow from './XItemRow';
 import XEditPanel from './XEditPanel';
 
 window.customElements.define('x-app', XApp);
+window.customElements.define('x-header', XHeader);
 window.customElements.define('x-button', XButton);
 window.customElements.define('x-item', XItem);
 window.customElements.define('x-item-container', XItemContainer);
